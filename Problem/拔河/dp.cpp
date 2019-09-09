6e30727589d6bd36e47ee10ec3510ade87d85810
qzylalala <43845521+qzylalala@users.noreply.github.com>
2019-09-09 16:25:28 +0800

    Update dp.cpp

--- Problem/拔河/dp.cpp
+++ Problem/拔河/dp.cpp
@@ -1,75 +1,34 @@
 /*http://codeup.cn/problem.php?id=1130*/
-#include <stdio.h>
-#include <string.h>
-#define maxn 500+1
-int dp[55][55 * 510] = { 0 };
-int p[maxn] = { 0 };
-
-int min(int a, int b) {
-	return a < b ? a : b;
-}
-int max(int a, int b) {
-	return a > b ? a : b;
-}
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+const int maxn = 450*105;
+int dp[105][maxn], a[105], n;
 int main()
 {
-	int n;
-	while (scanf("%d", &n) != EOF)
-	{
-
-		memset(dp, 0, sizeof(dp));
-		int sum = 0;
-		for (int i = 1; i <= n; i++)
-		{
-			scanf("%d", &p[i]);
-			sum += p[i];
-		}//先求总重量sum
-		int N = n / 2 + (n % 2 == 0 ? 0 : 1);
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = min(N, i); j >= 1; j--)
-			{
-				for (int k = sum / 2; k >= 0; k--)
-				{
-
-					if (k - p[i] >= 0)
-					{
-						if (j - 1 == 0)
-							dp[j][k] = (k == p[i] ? p[i] : 0);
-						else
-							if (dp[j - 1][k - p[i]] != 0)
-								dp[j][k] = dp[j - 1][k - p[i]] + p[i];
-					}
-
-				}
-
-			}
-
-		}
-		int w;
-		for (int i = sum / 2; i >= 0; i--)
-		{
-			if (dp[N][i] != 0)
-			{
-				w = dp[N][i];
-				break;
-			}
-		}
-		int qq;
-		if (w > sum - w)
-		{
-			qq = sum - w;
-		}
-		else
-		{
-			qq = w;
-			w = sum - w;
-
-
-		}
-		printf("%d %d\n", qq, w);
-
-
-	}
-	return 0;
+    while(~scanf("%d", &n))
+    {
+        memset(dp, 0, sizeof(dp));
+        int limit1, limit2, sum = 0;
+        for(int i = 1; i <= n; i++)
+            scanf("%d", &a[i]), sum += a[i];
+        limit1 = n/2;
+        limit2 = n/2 + n%2;
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++)
+            for(int j = i; j >= 1; j--)
+                for(int v = sum; v >= a[i]; v--)
+                    if(dp[j-1][v-a[i]]) dp[j][v] = 1;
+        int temp = 1e9;
+        int k = sum/2+1;
+        while(k--)
+        {
+            if(dp[limit1][k] || dp[limit2][k])
+                break;
+        }
+        printf("%d %d\n", k, sum-k);
+    }
+    return 0;
 }
